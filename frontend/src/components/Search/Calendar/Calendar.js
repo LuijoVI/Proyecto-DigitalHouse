6b9c55cb37a0f77a52cc020d48681c59de91d147
LuisJoseVI <l.villalobos@urbe.edu.ve>
2025-09-05 15:25:04 -0400

    filtros lugar y fecha

--- frontend/src/components/Search/Calendar/Calendar.js
+++ frontend/src/components/Search/Calendar/Calendar.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import './Calendar.css';
+import selectCityStyle from '../SelectCity/SelectCity.module.css';
 import es from 'date-fns/locale/es';
 import useWindowSize from '../../../hooks/useWindowSize';
 import { dateRangeContext } from '../../../context/DateRangeContext';
 import { useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 
 const Calendar = ({ inline, reserved }) => {
   const { dateRangeCapture } = useContext(dateRangeContext);
@@ -26,6 +28,18 @@ const Calendar = ({ inline, reserved }) => {
     };
   });
 
+ const CalendarInput = React.forwardRef(({ value, onClick, className }, ref) => (
+  <div
+    className={selectCityStyle.dropDownHeader}
+    onClick={onClick}
+    ref={ref}
+    style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}
+  >
+    <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px', width: '20px', height: '25px' }} />
+    <span style={{ fontSize: '14px', fontWeight: 500, color: '#7f7f7f' }}>{value || "Fecha de reserva"}</span>
+  </div>
+));
+
   // console.log(dateBooking);
 
   return (
@@ -36,7 +50,7 @@ const Calendar = ({ inline, reserved }) => {
       minDate={new Date()}
       monthsShown={width < 768 ? 1 : 2}
       dateFormat="dd/MM/yyyy"
-      placeholderText="Fecha de inicio - Fecha de finalización"
+      customInput={<CalendarInput />}
       onChange={(update) => {
         setDateRange(update);
       }}
